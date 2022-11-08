bb06a0ff480beaec77833dda7c69bbe6bc145875
DavidCA01 <00386222@uca.edu.sv>
2022-11-08 14:42:31 -0600

    Lab08

---
+++ LAB08/ejemplo1.c
@@ -0,0 +1,12 @@
+// 1.	¿Qué es ser autodidacta?
+// R: Ser autodidacta, para mi es una persona que puede aprender y realiza estudios por su propia cuenta.
+// 2.	Investigar que son matrices (definición, sintaxis, etc.)
+// R: Un arreglo (matriz) es una colección ordenada de datos (tanto primitivos u objetos dependiendo del lenguaje). 
+// Los arreglos (matrices) se emplean para almacenar múltiples valores en una sola variable, 
+// frente a las variables que sólo pueden almacenar un valor (por cada variable), tienen columnas, pero también múltiples filas, 
+// esta vez de color amarillo. Para facilitar el acceso a las distintas posiciones hemos dispuesto también: 
+// El cursor que indica visualmente qué posición (fila y columna) está activa en cada momento.
+// 3. Listar 3 fuentes de donde aprender a programar (canales de YouTube, foros, páginas, etc.
+// R: https://learn.microsoft.com/es-es/training/modules/web-development-101-introduction-programming/
+// https://www.youtube.com/channel/UC-aSfQ1bPJNZGQSRsA1xGMA
+// https://www.youtube.com/c/HolaMundoDev
